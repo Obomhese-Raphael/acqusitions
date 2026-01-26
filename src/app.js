8949f4cc7b5835a7a8256f0cda20933985e0b598
Raphael Obomhese <obomheser@gmail.com>
2026-01-26 11:14:05 +0100

    Implement eslint and prettier

--- src/app.js
+++ src/app.js
@@ -1,11 +1,9 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
-
-app.get("/", (req, res) => {
-  res.status(200).send("Welcome to the Acquisitions Service");
+app.get('/', (req, res) => {
+  res.status(200).send('Welcome to the Acquisitions Service');
 });
 
 export default app;
