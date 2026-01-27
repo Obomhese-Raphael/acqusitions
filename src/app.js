eac0b335fa8a14637381092c7a9514af592272a9
Raphael Obomhese <obomheser@gmail.com>
2026-01-27 13:06:59 +0100

    Secured API with Arcjet

--- src/app.js
+++ src/app.js
@@ -5,8 +5,11 @@ import morgan from 'morgan';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import authRoutes from '#routes/auth.routes.js';
+import securityMiddleware from '#middleware/security.middleware.js';
+// import usersRoutes from '#routes/users.routes.js';
 
 const app = express();
+
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
@@ -19,24 +22,33 @@ app.use(
   })
 );
 
-app.get('/', (req, res) => {
-  logger.info('Hello from Acqusitions!');
+app.use(securityMiddleware);
 
-  res.status(200).send('Welcome to the Acquisitions Service');
+app.get('/', (req, res) => {
+  logger.info('Hello from Acquisitions!');
+
+  res.status(200).send('Hello from Acquisitions!');
 });
 
 app.get('/health', (req, res) => {
-  res.status(200).json({
-    status: 'OK',
-    timestamp: new Date().toISOString(),
-    uptime: process.uptime(),
-  });
+  res
+    .status(200)
+    .json({
+      status: 'OK',
+      timestamp: new Date().toISOString(),
+      uptime: process.uptime(),
+    });
 });
 
 app.get('/api', (req, res) => {
   res.status(200).json({ message: 'Acquisitions API is running!' });
 });
 
-app.use('/api/auth', authRoutes); //api/auth/sign-in
+app.use('/api/auth', authRoutes);
+// app.use('/api/users', usersRoutes);
+
+app.use((req, res) => {
+  res.status(404).json({ error: 'Route not found' });
+});
 
 export default app;
